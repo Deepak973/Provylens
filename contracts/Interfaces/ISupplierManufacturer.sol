934ae3ab46077bf0da2d810192073fc5811fe4cb
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-04-12 14:31:10 +0530

    product transfer logic changed

--- contracts/Interfaces/ISupplierManufacturer.sol
+++ contracts/Interfaces/ISupplierManufacturer.sol
@@ -2,12 +2,23 @@
 pragma solidity >=0.8.0 <=0.8.19;
 
 interface ISupplierManufacturer {
+  enum transferStatus {
+    none,
+    Requested,  //1
+    Approved,   //2
+    Received    //3
+  } 
+
   struct supplierManufacturer{
+      uint smId;
       uint spId;
       address supplierAddress;
       address manufacturerAddress;
       uint32 dispatchTime;
       uint32 arrivalTime;
+      uint32 quantity;
+      uint32 currentQuantity;
+      transferStatus status;
   }
 
   struct requestDetails{
@@ -15,12 +26,13 @@ interface ISupplierManufacturer {
       address supplierAddress;
       address manufacturerAddress;
       uint quantity;
-
+      transferStatus transferStatus;
   }
 
   event eventSupplierManufacturerTransfer(uint indexed _smId,uint indexed _spId, address _supplierAddress,address _manufacturerAddress,uint32 _dispatchTime);
   event eventArrivalTime(uint32 _arrivalTime);
   
-  function transferProduct(uint _spId, address _supplierAddress,address _manufacturerAddress,uint32 _dispatchTime)external;
+  function transferProduct(uint _spId, address _manufacturerAddress,uint32 _quantity,uint32 _currentQuantity)external;
+  function requestProduct(uint _spId,uint32 _quantity, address _supplierAddress)external;
   function receiveProduct(uint smId)external;
 }
--- contracts/supplierManufacturer.sol
+++ contracts/supplierManufacturer.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 import "./Interfaces/ISupplierManufacturer.sol";
+import "./userDetails.sol";
+import "./supplierProduct.sol";
 pragma solidity >=0.8.0 <=0.8.19;
 
 /// @author ProvyLens team
@@ -10,87 +12,96 @@ contract supplierManufacturer is ISupplierManufacturer{
     uint public smId = 1;
     uint public reqId =1;
 
+    userDetails ud = userDetails(0xd9145CCE52D386f254917e481eB44e9943F39138);
+    supplierProduct spInstance = supplierProduct(0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47);
+
+    function getAllSupplierAddresses() public view returns(address[] memory){
+        return ud.getAllSupplierAddresses();
+    }
+
     // struct
         //   uint sp_id;
         //   address supplierAddress;
         //   address manufacturerAddress;
         //   uint32 dispatchTime;
         //   uint32 arrivalTime;
+        //   uint32 quantity;
+        //   uint32 currentQuantity;
+        //   transferStatus _status;
 
     // events
         // eventSupplierManufacturerTransfer
         // eventArrivalTime
 
+    // enum transferStatus
+        // none,
+        // Requested,  //1
+        // Approved,   //2
+        // Received    //3
+
     /// @notice mapping smId index with supplierManufacturer
     mapping(uint => supplierManufacturer) public smIdToStructMapping;
-    mapping(uint => requestDetails) public reqIdToDetailsMapping;
-    mapping(address => uint[]) public supplierToRequestMapping;
-    mapping(address => uint[]) public manufacturerToRequestMapping;
+   
 
-
-    /// @notice mapping address with all the created smIds
     mapping(address => uint[]) public manufacturerTosmIdMapping;
 
-    /// @notice function to transfer product from supplier to manufacturer
-    function transferProduct(uint _spId, address _supplierAddress,address _manufacturerAddress,uint32 _dispatchTime)external override{
-        smIdToStructMapping[smId] = supplierManufacturer(_spId,_supplierAddress,_manufacturerAddress,_dispatchTime,0);
-        manufacturerTosmIdMapping[_manufacturerAddress].push(smId);
-        emit eventSupplierManufacturerTransfer(smId,_spId,_supplierAddress,_manufacturerAddress,_dispatchTime);
-        smId++;
-    } 
+    mapping(address => uint[]) public supplierTosmIdMapping;
 
+
+
+    /// @notice function to transfer product from supplier to manufacturer
+    function transferProduct(uint _spId,address _manufacturerAddress,uint32 _quantity,uint32 _currentQuantity)external override{
+        userDetails.userDetails memory user = ud.getSingleUser(msg.sender);
+        require(uint8(user.userType)== 0,"Only Supplier can Transfer product"); 
+
+        smIdToStructMapping[smId].spId = _spId;
+        smIdToStructMapping[smId].supplierAddress = msg.sender;
+        smIdToStructMapping[smId].dispatchTime = uint32(block.timestamp);
+        smIdToStructMapping[smId].currentQuantity = _currentQuantity;
+        smIdToStructMapping[smId].status = transferStatus.Approved;
+        
+        emit eventSupplierManufacturerTransfer(smId,_spId,msg.sender,_manufacturerAddress,uint32(block.timestamp));
+        spInstance.updateSupplierProductUints(_spId,_quantity);
+        
+    }
+
+    function requestProduct(uint _spId,uint32 _quantity, address _supplierAddress)external override {
+        userDetails.userDetails memory user = ud.getSingleUser(msg.sender);
+        require(uint8(user.userType)== 1,"Only Manufacturer can Request product"); 
+
+        smIdToStructMapping[smId] = supplierManufacturer(smId,_spId,_supplierAddress,msg.sender,0,0,_quantity,0,transferStatus.Requested);
+        manufacturerTosmIdMapping[msg.sender].push(smId);
+        supplierTosmIdMapping[msg.sender].push(smId);
+        smId++;
+    }
+    
     /// @notice function to update that the product has been received
     function receiveProduct(uint _smId)external override{
+        userDetails.userDetails memory user = ud.getSingleUser(msg.sender);
+        require(uint8(user.userType)== 1,"Only Manufacturer can acknowledge receive product"); 
         smIdToStructMapping[_smId].arrivalTime = uint32(block.timestamp);
+        smIdToStructMapping[_smId].status = transferStatus.Received;
         emit eventArrivalTime(uint32(block.timestamp));
     } 
 
     /// @notice function to get single product transfer information
-    function getProduct(uint _smId) public view returns(supplierManufacturer memory){
+    function getSmDetails(uint _smId) public view returns(supplierManufacturer memory){
         return smIdToStructMapping[_smId];
     }
-
-
-    /// @notice function to request product from supplier
-
-    function requestproduct(uint _spId,address _supplierAddress,uint _quantity) public
-    {
-        reqIdToDetailsMapping[reqId] =requestDetails(_spId,_supplierAddress,msg.sender,_quantity);
-        supplierToRequestMapping[_supplierAddress].push(reqId);
-        manufacturerToRequestMapping[msg.sender].push(reqId);
-        reqId++;
-    }
-
-
-    /// @notice funtion to get all request from a manufacturer
-
-    function getSupplierRequests(address _supplierAddress) public view returns(requestDetails[] memory)
-    {
-        requestDetails[] memory allRequestDetails = new requestDetails[](reqId);
-        uint[] memory reqIds = supplierToRequestMapping[_supplierAddress];
-        for(uint i=0;i<reqIds.length;i++)
-        {
-            allRequestDetails[i] = reqIdToDetailsMapping[reqIds[i]];
-        }
-        return allRequestDetails;
-    }
-
-    function getManufacturerRequests(address _manufacturerAddress) public view returns(requestDetails[] memory)
-    {
-        requestDetails[] memory allRequestDetails = new requestDetails[](reqId);
-        uint[] memory reqIds = manufacturerToRequestMapping[_manufacturerAddress];
-        for(uint i=0;i<reqIds.length;i++)
-        {
-            allRequestDetails[i] = reqIdToDetailsMapping[reqIds[i]];
-        }
-        return allRequestDetails;
-    }
-
-    
 
     /// @notice function to get all trasnfers manufacturer has received
     function getAllSmIdForManufacturer(address _manufacturerAddress) public view returns(supplierManufacturer[] memory){
         uint[] memory smIdData = manufacturerTosmIdMapping[_manufacturerAddress];
+        supplierManufacturer[] memory smIdDetails = new supplierManufacturer[](smIdData.length);
+        for(uint i=0;i<smIdData.length;i++)
+        {
+            smIdDetails[i] = smIdToStructMapping[smIdData[i]];
+        }
+        return smIdDetails;
+    }
+
+    function getAllSmIdForSupplier(address _supplierAddress) public view returns(supplierManufacturer[] memory){
+        uint[] memory smIdData = supplierTosmIdMapping[_supplierAddress];
         supplierManufacturer[] memory smIdDetails = new supplierManufacturer[](smIdData.length);
         for(uint i=0;i<smIdData.length;i++)
         {
