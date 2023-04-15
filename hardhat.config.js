680faa1cec278c2065149548a5b20727befc9003
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2023-04-15 18:20:34 +0530

    contract verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config({ path: __dirname + "/.env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -8,12 +9,22 @@ module.exports = {
     artifacts: "./src/artifacts",
   },
   networks: {
+    customChains: [
+      {
+        network: "bttc",
+        chainId: 1029,
+        urls: {
+          apiURL: "https://bttcscan.com",
+          browserURL: "https://testnet.bttcscan.com/",
+        },
+      },
+    ],
     bttc: {
       url: process.env.API_KEY_URL, //Your RPC URL
       accounts: [process.env.PRIVATE_KEY], //Your private key
     },
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
+    apiKey: process.env.BTTCSCAN_API_KEY,
   },
 };
